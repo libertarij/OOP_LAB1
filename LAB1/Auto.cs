901ced423a27e0dff7ef56579b210933bf5a8899
libertarij <Gtr_#6+Tht>
2020-04-12 13:11:20 +0300

    Добавьте файлы проекта.

---
+++ LAB1/Auto.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    class Auto
+    {
+        public string brand;
+        public string model;
+        private int tank_volume;
+        private int qty_gasoline;
+        private int speed;
+        private double fuel_cons;
+        private double[] wheel_preassure = new double[4];
+
+        public int Tank_volume
+        {
+            get
+            {
+                return tank_volume;
+            }
+
+            set
+            {
+                if (value > 0 & value < 2000)
+                    tank_volume = value;
+            }
+        }
+
+        public int Qty_gasoline
+        {
+            get
+            {
+                return qty_gasoline;
+            }
+
+            set
+            {
+                if (value >= 0 & value < 2000)
+                    qty_gasoline = value;
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                if (value >= 0 & value < 300)
+                    speed = value;
+            }
+        }
+
+        public double Fuel_cons
+        {
+            get
+            {
+                return fuel_cons;
+            }
+
+            set
+            {
+                if (value >= 0 & value < 30)
+                    fuel_cons = value;
+            }
+        }
+
+        public double[] Wheel_preassure
+        {
+            get
+            {
+                return wheel_preassure;
+            }
+
+            set
+            {
+                wheel_preassure.Initialize(); //Чистка массива
+                int i = 0;
+                foreach (double m in value)
+                {
+                    wheel_preassure[i] = m;
+                    i++;
+                    if (i >= 4) break;
+                }
+            }
+        }
+
+        public Auto() : this("Nobrand", "Nomodel", 1, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        {
+        }
+
+        public Auto(string brand, string model, int tank_volume, int qty_gasoline, int speed, double fuel_cons, params double[] wheel_preassure)
+        {
+            this.brand = brand;
+            this.model = model;
+            Tank_volume = tank_volume;
+            Qty_gasoline = qty_gasoline;
+            Speed = speed;
+            Fuel_cons = fuel_cons;
+            Wheel_preassure = wheel_preassure;
+        }
+
+        public Auto(string brand, string model):this(brand, model, 1, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        {
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Марка: {0},\nмодель: {1},\nобъем бака: {2} л.,\nкол-во топлива в баке: {3} л.,\nскорость: {4} км/ч,\nрасход: {5} л/100км,\nдавление в:\n-1-ом колесе: {6} Атм.,\n-2-ом колесе: {7} Атм.,\n-3-ом колесе: {8} Атм.,\n-4-ом колесе: {9} Атм.", brand, model, tank_volume, qty_gasoline, speed, fuel_cons, wheel_preassure[0], wheel_preassure[1], wheel_preassure[2], wheel_preassure[3]);
+        }
+
+        public int Distance()
+        {
+            return (int)(this.qty_gasoline * 100 / this.fuel_cons);
+        }
+
+        public bool Pen_Wheel()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (wheel_preassure[i] < 1.0)
+                {
+                    speed = 0;
+                   return  true;
+                }
+            }
+            return false;
+        }
+
+        public bool Faster(Auto auto2)
+        {
+            return this.speed - auto2.speed > 0;
+        }
+
+        public static Auto Faster_of_3(Auto auto1, Auto auto2, Auto auto3)
+        {
+            if (auto1.speed > auto2.speed & auto1.speed > auto3.speed)
+            {
+                return auto1;
+            }
+            else if (auto2.speed > auto3.speed)
+            {
+                return auto2;
+            }
+            else
+            {
+                return auto3;
+            }
+        }
+    }
+}
---
+++ LAB1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Демонстрация использования всех конструкторов
+            Console.WriteLine("Демонстрация использования всех конструкторов:");
+            Console.WriteLine("1. Создание объекта класса Auto с помощью пустого коснтруктора:");
+            Auto auto1 = new Auto();
+            Console.WriteLine(auto1);
+            Console.WriteLine("");
+
+            Console.WriteLine("2. Создание объекта класса Auto, конструктором с полным набором аргументов:");
+            Auto auto2 = new Auto("Mercedes", "ML370", 70, 30, 40, 8.2, 2.2, 2.2, 2.3, 0.9);
+            Console.WriteLine(auto2);
+            Console.WriteLine("");
+
+            Console.WriteLine("3. Создание объекта класса Auto, конструктором с частичным набором аргументов:");
+            Auto auto3 = new Auto("Mercedes", "ML270");
+            Console.WriteLine(auto3);
+            Console.WriteLine("");
+
+            //Демонстрация работы методов
+            Console.WriteLine("Демонстрация работы методов:");
+            Console.WriteLine("1. Определение дистанции которое еще может проехать автомобиль: {0}", auto2.Distance());
+            Console.WriteLine("2. Определение разницы скорости между 2-мя автомобилями второй автомобиль быстрее первого? {0}", auto2.Faster(auto1));
+            Console.WriteLine("3. Определение более быстрого автомобиля из 3-х:\n{0}", Auto.Faster_of_3(auto1, auto2, auto3));
+           ;
+            Console.WriteLine("4. Проверка колес на прокол, и в случае прокола остановка автомобиля.\nКолесо пробыто? {0}.\nСкорость авто после проверки = {1}", auto2.Pen_Wheel(),  auto2.Speed);
+            Console.WriteLine("");
+
+            //Демонстрация работы свойств
+            Console.WriteLine("Демонстрация работы свойств:");
+            //Получение значений
+            Console.WriteLine("Получение значений для auto2");
+            Console.WriteLine("Марка автомобиля: " + auto2.brand);
+            Console.WriteLine("Расход в л на 100 км: " + auto2.Fuel_cons);
+            Console.WriteLine("Модель автомобиля: " + auto2.model);
+            Console.WriteLine("Количество оставшегося топлива: " + auto2.Qty_gasoline);
+            Console.WriteLine("Скорость автомобиля: " + auto2.Speed);
+            Console.WriteLine("Объем бака: " + auto2.Tank_volume);
+            Console.WriteLine("Давление в 4-х колесах: " + auto2.Wheel_preassure[0] + ", " + auto2.Wheel_preassure[1] + ", " + auto2.Wheel_preassure[2] + ", " + auto2.Wheel_preassure[3]);
+            Console.WriteLine("");
+
+            //Присваивание значений
+            Console.WriteLine("Присваивание значений");
+            auto1.brand = "BMW";
+            auto1.Fuel_cons = 9.2;
+            auto1.model = "X5";
+            auto1.Qty_gasoline = 10;
+            auto1.Speed = 20;
+            auto1.Tank_volume = 60;
+            auto1.Wheel_preassure = new double[] { 2.1, 2.1, 2.2, 2.2 };
+            Console.WriteLine(auto1);
+
+            Console.ReadKey();
+        }
+    }
+}
